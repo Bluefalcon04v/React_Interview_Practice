4ec66fdbb8242f3006a6a84d17d2ff7f8c5b64a2
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-04-25 13:50:48 +0530

    implementing the useState hook in functional comp.

--- React-Questions/src/Components/Hooks/useState/useState_FunctionalComponent.jsx
+++ React-Questions/src/Components/Hooks/useState/useState_FunctionalComponent.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 const useState_FunctionalComponent = () => {
-    console.log('useState_FunctionalComponent')
+    const [count, setCount] = useState(0)       // here 0 is the default value in useState & count and setCount are the parameters to be passed.
+
+    const increment = () => {
+        setCount(count+1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
   return (
     <div>
+        <p>useState Hook in Functional Component</p>
+        <p> Count: {count} </p>  
+        <button onClick={increment}> + </button>
+        <button onClick={decrement}> - </button>
     </div>
   )
 }
