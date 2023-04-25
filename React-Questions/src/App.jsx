b12cff1c9df8393bceff0bba06a4654eabbd5565
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-04-25 12:57:18 +0530

    First commit

---
+++ React-Questions/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import UseState from './Components/Hooks/useState/useState_FunctionalComponent'
+
+function App() {
+
+  return (
+    <div className="container">
+      <UseState/>
+    </div>
+  )
+}
+
+export default App
---
+++ React-Questions/src/Components/Hooks/useState/useState_FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useState_FunctionalComponent = () => {
+    console.log('useState_FunctionalComponent')
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default useState_FunctionalComponent
